c601c3a31a397b3ac7ecbf5153323809545c10fb
NakazatoTaichi <taichi.nakazoto0521@gmail.com>
2024-01-01 00:47:05 +0900

    新規登録画面のデザイン修正

--- resources/views/register.blade.php
+++ resources/views/register.blade.php
@@ -7,32 +7,39 @@
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-9ndCyUaIbzAi2FUVXJi0CjmCapSmO7SnpJef0486qhLnuZ2cdeRhO02iuK6FUUVM" crossorigin="anonymous">
     <title>登録画面</title>
 </head>
-<body class="bg-light">
+<body>
     <div class="container mt-5">
         <div class="row justify-content-center">
-            <div class="col-md-6">
-                <h1 class="text-center mb-4">登録画面</h1>
-                <form action="{{ route('register') }}" method="post" enctype="multipart/form-data">
-                    @csrf
-                    <div class="form-group mb-3">
-                        <label for="name">名前</label>
-                        <input type="text" class="form-control" name="name" id="name">
+            <div class="col-md-8 bg-light">
+                <div class="row justify-content-center">
+                    <div class="col-md-8">
+                        <h1 class="text-center my-4">新規登録</h1>
+                        <hr style="border: 3px solid #8c8b8b;">
+                        <form action="{{ route('register') }}" method="post" enctype="multipart/form-data">
+                            @csrf
+                            <div class="form-group mb-3" style="font-size: 1.5rem;">
+                                <label for="name">名前</label>
+                                <input type="text" class="form-control" name="name" id="name">
+                            </div>
+                            <div class="form-group mb-3" style="font-size: 1.5rem;">
+                                <label for="email">メールアドレス</label>
+                                <input type="email" class="form-control" name="email" id="email">
+                            </div>
+                            <div class="form-group mb-3" style="font-size: 1.5rem;">
+                                <label for="password">パスワード</label>
+                                <input type="password" class="form-control" name="password" id="password">
+                            </div>
+                            <div class="form-group mb-3" style="font-size: 1.5rem; height: 250px;">
+                                <label for="icon">アイコン画像</label>
+                                <input type="file" class="form-control-file" name="icon" id="icon" onchange="previewImage()">
+                                <img id="preview">
+                            </div>
+                            <div class="mb-4 text-center">
+                                <button type="submit" class="btn btn-primary btn-block" style="font-size: 1.5rem; width: 150px;">登録</button>
+                            </div>
+                        </form>
                     </div>
-                    <div class="form-group mb-3">
-                        <label for="email">メールアドレス</label>
-                        <input type="email" class="form-control" name="email" id="email">
-                    </div>
-                    <div class="form-group mb-3">
-                        <label for="password">パスワード</label>
-                        <input type="password" class="form-control" name="password" id="password">
-                    </div>
-                    <div class="form-group mb-3">
-                        <label for="icon">アイコン画像</label>
-                        <input type="file" class="form-control-file" name="icon" id="icon" onchange="previewImage()">
-                        <img id="preview" src="#" alt="アイコンプレビュー">
-                    </div>
-                    <button type="submit" class="btn btn-primary btn-block">送信</button>
-                </form>
+                </div>
             </div>
         </div>
     </div>
