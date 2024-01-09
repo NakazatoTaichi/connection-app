291ed429d00075e50f317580ed5cda7d23a7dc71
NakazatoTaichi <taichi.nakazoto0521@gmail.com>
2024-01-10 06:01:12 +0900

    ロゴが表示されるようにした

--- resources/views/layouts/layout.blade.php
+++ resources/views/layouts/layout.blade.php
@@ -9,17 +9,14 @@
 
     <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js"></script>
     <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.10.2/dist/umd/popper.min.js"></script>
-    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.min.js"></script>
 </head>
 <body>
     <div class="header">
-        <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
-            <div class="app-logo">
-                <img src="file:///Users/nakazatotaichi/laravel_docker/connection-app/connection-app.png" alt="">
+        <nav class="navbar navbar-expand-lg text-center" style="font-size: 1.5rem;">
+            <div class="navbar-brand">
+                <img src="{{ asset('img/connection-app.png') }}" alt="logo" width="100" height="100">
             </div>
-            <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-                <span class="navbar-toggler-icon"></span>
-            </button>
             <div class="collapse navbar-collapse" id="navbarNav">
                 <ul class="navbar-nav">
                     <li class="nav-item"><a class="nav-link" href="#">ホーム</a></li>
@@ -27,8 +24,15 @@
                     <li class="nav-item"><a class="nav-link" href="#">投稿</a></li>
                     <li class="nav-item"><a class="nav-link" href="#">マイページ</a></li>
                 </ul>
-                <div class="login">
-                    <p>ログイン者：〇〇〇〇</p>
+                <div class="dropdown">
+                    <a class="dropdown-toggle" href="#" role="button" data-toggle="dropdown" aria-expanded="false">
+                        ログイン者
+                    </a>
+                    <ul class="dropdown-menu">
+                        <li><a class="dropdown-item" href="#">Action</a></li>
+                        <li><a class="dropdown-item" href="#">Another action</a></li>
+                        <li><a class="dropdown-item" href="#">Something else here</a></li>
+                    </ul>
                 </div>
             </div>
         </nav>
@@ -37,57 +41,49 @@
         @yield('main')
     </div>
 <style>
-nav {
+.navbar {
     background-color: #333;
     overflow: hidden;
-    height: 100px;
-    display: flex;
-    align-items: center;
     position: fixed;
+    text-item: center;
     width: 100%;
     top: 0;
     z-index: 100;
 }
 
-.app-logo img {
-    margin-top: 3px;
-    width: 100px;
+.navbar-brand {
+    margin: 0;
+    padding: 0 8px;
 }
 
-nav ul {
+.navbar-nav {
     list-style: none;
     padding: 0;
     margin: 0;
 }
 
-nav li {
+.nav-item {
     float: left;
 }
 
-nav a {
-
+.nav-link {
     display: block;
     color: white;
-    text-align: center;
     width: 150px;
     margin-left: 20px;
-    padding: 10px 12px;
+    padding: 10px 8px;
     text-decoration: none;
     border-radius: 30px;
     border: 2px solid white;
-    font-size: 1.5rem;
 }
 
-nav a:hover {
+.nav-link:hover {
     background-color: #ddd;
     color: black;
 }
 
-.login {
-    margin-left: auto;
-    color: white;
-    text-align: center;
-    padding: 30px;
+.main {
+    margin-top: 116px;
 }
 </style>
 </body>
