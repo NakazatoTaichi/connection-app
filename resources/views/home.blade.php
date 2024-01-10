13d71febb353bbf21412cbabe9e04fad34f37968
NakazatoTaichi <taichi.nakazoto0521@gmail.com>
2024-01-10 22:07:08 +0900

    コメントアウト消した

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -5,18 +5,6 @@
 @section('main')
     <div class="container pt-5">
         <h1>Home</h1>
-        {{-- <div class="my-4">
-            @if ($user->icon)
-                <img src="{{ asset('storage/icons/' . $user->icon )}}" alt="icon" class="img-fluid rounded-circle" style="width: 50px; height: 50px;">
-            @else
-                <div class="icon-placeholder rounded-circle" style="width: 50px; height: 50px; background-color: #CCCCCC;"></div>
-            @endif
-            {{\Illuminate\Support\Facades\Auth::user()->name}}でログインしています。
-        </div>
-        <form action="{{route('user.logout')}}" method="post">
-            @csrf
-            <button>ログアウト</button>
-        </form> --}}
         <div class="row mt-3">
             <div class="col-md-3">
                 <a href="{{ route('user.index') }}">ユーザ一覧</a>
