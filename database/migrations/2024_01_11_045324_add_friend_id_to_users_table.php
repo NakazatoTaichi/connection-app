06bb57808a488603a1f9ede5f41c9753bf9114a6
NakazatoTaichi <taichi.nakazoto0521@gmail.com>
2024-01-11 06:04:58 +0900

    ユーザに8桁の重複しないランダムな数字をフレンドIDとして付与した

---
+++ database/migrations/2024_01_11_045324_add_friend_id_to_users_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::table('users', function (Blueprint $table) {
+            $table->unsignedInteger('friend_id')->unique()->nullable();
+        });
+
+        $users = \App\Models\User::all();
+        foreach ($users as $user) {
+            $user->update(['friend_id' => mt_rand(10000000, 99999999)]);
+        }
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::table('users', function (Blueprint $table) {
+            $table->dropColumn('friend_id');
+        });
+    }
+};
