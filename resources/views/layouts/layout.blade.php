6a750840e7a79824227ff61ff6c192327265ee49
NakazatoTaichi <taichi.nakazoto0521@gmail.com>
2024-01-10 21:39:40 +0900

    navbarのレイアウトを整えた＆ログアウトできるようにした

--- resources/views/layouts/layout.blade.php
+++ resources/views/layouts/layout.blade.php
@@ -7,7 +7,7 @@
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-9ndCyUaIbzAi2FUVXJi0CjmCapSmO7SnpJef0486qhLnuZ2cdeRhO02iuK6FUUVM" crossorigin="anonymous">
     <title>@yield('title')</title>
 
-    <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/jquery@3.5.1/dist/jquery.min.js"></script>
     <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.10.2/dist/umd/popper.min.js"></script>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.min.js"></script>
 </head>
@@ -19,19 +19,31 @@
             </div>
             <div class="collapse navbar-collapse" id="navbarNav">
                 <ul class="navbar-nav">
-                    <li class="nav-item"><a class="nav-link" href="#">ホーム</a></li>
-                    <li class="nav-item"><a class="nav-link" href="#">トーク</a></li>
+                    <li class="nav-item"><a class="nav-link" href="{{ route('home')}}">ホーム</a></li>
+                    <li class="nav-item"><a class="nav-link" href="{{ route('user.index') }}">トーク</a></li>
                     <li class="nav-item"><a class="nav-link" href="#">投稿</a></li>
-                    <li class="nav-item"><a class="nav-link" href="#">マイページ</a></li>
+                    <li class="nav-item"><a class="nav-link" href="{{ route('myProfile') }}">マイページ</a></li>
                 </ul>
                 <div class="dropdown">
-                    <a class="dropdown-toggle" href="#" role="button" data-toggle="dropdown" aria-expanded="false">
-                        ログイン者
+                    @if (\Illuminate\Support\Facades\Auth::user()->icon)
+                        <img src="{{ asset('storage/icons/' . \Illuminate\Support\Facades\Auth::user()->icon )}}" alt="icon" class="img-fluid rounded-circle" style="width: 50px; height: 50px;">
+                    @elseif (\Illuminate\Support\Facades\Auth::user()->icon->null)
+                        <div class="icon-placeholder rounded-circle" style="width: 50px; height: 50px; background-color: #CCCCCC;"></div>
+                    @else
+                        <div class="icon-placeholder rounded-circle" style="width: 50px; height: 50px; background-color: #CCCCCC;"></div>
+                    @endif
+                    <a class="dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                        ログイン者：{{\Illuminate\Support\Facades\Auth::user()->name}}
                     </a>
                     <ul class="dropdown-menu">
-                        <li><a class="dropdown-item" href="#">Action</a></li>
-                        <li><a class="dropdown-item" href="#">Another action</a></li>
-                        <li><a class="dropdown-item" href="#">Something else here</a></li>
+                        <li>
+                            <form action="{{route('user.logout')}}" method="post">
+                                <button type="submit" class="dropdown-item">
+                                    @csrf
+                                    ログアウト
+                                </button>
+                            </form>
+                        </li>
                     </ul>
                 </div>
             </div>
@@ -45,7 +57,7 @@
     background-color: #333;
     overflow: hidden;
     position: fixed;
-    text-item: center;
+    text-align: center;
     width: 100%;
     top: 0;
     z-index: 100;
@@ -58,8 +70,6 @@
 
 .navbar-nav {
     list-style: none;
-    padding: 0;
-    margin: 0;
 }
 
 .nav-item {
@@ -67,7 +77,7 @@
 }
 
 .nav-link {
-    display: block;
+    /* display: block; */
     color: white;
     width: 150px;
     margin-left: 20px;
@@ -80,6 +90,19 @@
 .nav-link:hover {
     background-color: #ddd;
     color: black;
+}
+
+.dropdown {
+    margin-left: auto !important;
+    padding-right: 30px;
+}
+
+.dropdown-toggle {
+    text-decoration: none;
+}
+
+.dropdown-item {
+    background-color: #ddd;
 }
 
 .main {
