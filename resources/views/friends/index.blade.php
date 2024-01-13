5f9c35c5390bf361c0a54c1473a93117ee3c6f7d
NakazatoTaichi <taichi.nakazoto0521@gmail.com>
2024-01-14 08:20:05 +0900

    友だち一覧画面のデザイン変えた

--- resources/views/friends/index.blade.php
+++ resources/views/friends/index.blade.php
@@ -5,13 +5,13 @@
 @section('main')
     <div class="container mt-5">
         <h1>友だち一覧</h1>
-        <div class="mt-3">
+        <div class="my-4">
             <a class="btn btn-primary" href="{{route('friend.friendRegister')}}">友だち登録</a>
         </div>
         <div class="friend-list mt-2">
             @forelse ($friends as $friend)
-            <div class="friend-wrapper mb-4 border p-3">
-                <div class="friend-container row align-items-center mb-4">
+            <div class="friend-wrapper mb-4 border border-dark-subtle rounded-3 p-3">
+                <div class="friend-container row align-items-center">
                     <div class="col-md-1">
                         @if ($friend->icon)
                         <img src="{{ asset('storage/icons/' . $friend->icon )}}" alt="icon" class="img-fluid rounded-circle" style="width: 50px; height: 50px;">
@@ -21,7 +21,7 @@
                     </div>
                     <div class="friend-info col-md-3">
                         <a href="{{ route('chat.show', ['user' => $friend->id]) }}">
-                            <p>{{$friend->name}}</p>
+                            <p style="margin: 0;">{{$friend->name}}</p>
                         </a>
                     </div>
                 </div>
