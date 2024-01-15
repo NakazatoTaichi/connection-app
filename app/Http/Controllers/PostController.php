70e7bbd89955d40ca22ee105e8396ea140ed7d58
NakazatoTaichi <taichi.nakazoto0521@gmail.com>
2024-01-15 11:39:04 +0900

    投稿一覧画面に投稿されたポストが並ぶようにした

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -12,7 +12,8 @@ class PostController extends Controller
 {
     public function index()
     {
-        return view('posts.index');
+        $posts = Post::all();
+        return view('posts.index', compact('posts'));
     }
 
     public function create()
