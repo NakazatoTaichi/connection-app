7db1d205df3ca1166dc8490f7fb9dcbaafa06aa7
NakazatoTaichi <taichi.nakazoto0521@gmail.com>
2024-01-27 11:15:21 +0900

    戻るボタンをアイコンにした

--- resources/views/chat.blade.php
+++ resources/views/chat.blade.php
@@ -7,7 +7,9 @@
     <div class="sender py-3 row" style="font-size: 1.5rem;">
         <div class="col-2">
             <a style="text-decoration:none;" href="{{ route('friend.index') }}">
-                戻る
+                <svg xmlns="http://www.w3.org/2000/svg" width="30" height="30" fill="currentColor" class="bi bi-arrow-left-square" viewBox="0 0 16 16">
+                    <path fill-rule="evenodd" d="M15 2a1 1 0 0 0-1-1H2a1 1 0 0 0-1 1v12a1 1 0 0 0 1 1h12a1 1 0 0 0 1-1V2zM0 2a2 2 0 0 1 2-2h12a2 2 0 0 1 2 2v12a2 2 0 0 1-2 2H2a2 2 0 0 1-2-2V2zm11.5 5.5a.5.5 0 0 1 0 1H5.707l2.147 2.146a.5.5 0 0 1-.708.708l-3-3a.5.5 0 0 1 0-.708l3-3a.5.5 0 1 1 .708.708L5.707 7.5H11.5z"/>
+                </svg>
             </a>
         </div>
         <div class="col-8 text-center">
